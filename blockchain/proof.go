a0defc78da5921e027b0e30c7458d004313d8e95
dnguyen379 <dnguyen379@gatech.edu>
2021-02-12 21:30:24 -0500

    fix typo

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -81,7 +81,7 @@ func (pow *ProofOfWork) Validate() bool {
 
 func ToHex(num int64) []byte {
     buff := new(bytes.Buffer)
-    err := binary.Write(buff, binary.bigEndian, num)
+    err := binary.Write(buff, binary.BigEndian, num)
     if err != nil {
         log.Panic(err)
     }
